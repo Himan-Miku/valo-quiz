3b3627b558ca4fe3a354671339e21de85142e07a
Himan-Miku <himanshu28.mj@gmail.com>
2023-03-04 17:26:53 +0530

    welcome message

--- src/index.ts
+++ src/index.ts
@@ -7,4 +7,28 @@ import gradient from "gradient-string";
 import inquirer from "inquirer";
 import { createSpinner } from "nanospinner";
 
-console.log(chalk.bgGray("Hi Mom"));
+let playerName;
+
+const sleep = (ms = 2000) => new Promise((r) => setTimeout(r, ms));
+
+async function welcome() {
+  const welcomeTitle = chalkAnimation.rainbow(
+    "Test Your Knowledge about Valorant and earn a Spotify Playlist of Valorant Songs \n"
+  );
+  await sleep();
+  welcomeTitle.stop();
+
+  console.log(`
+    ${chalk.bgBlue("How to Play \n")}
+    Choose a option from the given options
+    If your answer is right you will be given a ${chalk.bgHex("#6461c2")(
+      "nice looking pic"
+    )},
+    which you can even set as your social media pp XD jk 🙃
+    So ${chalk.red("Bina Kisi Bakchodi ke")} , ${chalk.green(
+    "Lets get Started"
+  )}
+  `);
+}
+
+await welcome();
