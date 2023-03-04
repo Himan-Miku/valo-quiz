0b9b3c4ac121013689e46331237f46ce8157d15b
Himan-Miku <himanshu28.mj@gmail.com>
2023-03-04 16:29:56 +0530

    import all dependencies

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,4 @@
-export {};
+#!/usr/bin/env node
+import chalk from "chalk";
+console.log(chalk.bgGray("Hi Mom"));
 //# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+
+import chalk from "chalk";
+import chalkAnimation from "chalk-animation";
+import figlet from "figlet";
+import gradient from "gradient-string";
+import inquirer from "inquirer";
+import { createSpinner } from "nanospinner";
+
+console.log(chalk.bgGray("Hi Mom"));
